c69fc5ee8fdcbffd44cbf7b2ae9a4e7ad5c91b82
Sang Han <rv498@hotmail.com>
2016-11-29 05:02:18 -0800

    m

--- assignment/SpeakHello.js
+++ assignment/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function() {
+(function(window) {
 
     // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
     // the "speak" method and which you will expose to the global context
@@ -15,7 +15,7 @@
     // STEP 4: Rewrite the 'speak' function such that it is attached to the
     // helloSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
-    helloSpeaker["speak"] = function speak(name) {
+    helloSpeaker.speak = function speak(name) {
         console.log(speakWord + " " + name);
     }
 
@@ -28,4 +28,4 @@
 
     // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
     // xxxx.xxxx = helloSpeaker;
-})();
+})(window);
